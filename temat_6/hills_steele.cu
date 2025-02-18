a8a59025794820bba641071598d112e862436651
Mkoek213 <mikolaj.kolek2@gmail.com>
2025-02-18 19:02:55 +0100

    temat 6 zrobiony

---
+++ temat_6/hills_steele.cu
@@ -0,0 +1,65 @@
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cassert>
+#include <algorithm>
+#include <numeric>
+
+template<typename T, typename Func>
+__global__ void hills_steele_scan(T* const result,const T* const source, Func func){
+    assert(gridDim.x == 1);
+    auto const tid = threadIdx.x;
+    result[tid] = source[tid];
+    __syncthreads();
+
+    for (auto stride = 1U; stride < blockDim.x; stride <<= 1){
+        if (tid >= stride){
+            result[tid] = func(result[tid], result[tid - stride]);
+        }
+        __syncthreads();
+    }
+}
+
+template<typename T>
+void showData(const T* const data, const size_t size){
+    std::for_each(data, data + size, [](T a){printf("%3d ", a);});
+    putchar('\n');
+}
+
+int main(){
+    using DataType_t = int;
+
+    constexpr static size_t Elements = 16;
+    constexpr static size_t MemElements = Elements * sizeof(DataType_t);
+    constexpr static size_t ThreadsInBlock = 16;
+    constexpr static size_t Block = (Elements + ThreadsInBlock - 1) / ThreadsInBlock;
+
+    DataType_t* hSource = static_cast<DataType_t *>(malloc(MemElements));
+    DataType_t* hDestination = static_cast<DataType_t *>(malloc(MemElements));
+
+    DataType_t* dSource = nullptr;
+    DataType_t* dDestination = nullptr;
+
+    cudaMalloc(&dSource, MemElements);
+    cudaMalloc(&dDestination, MemElements);
+
+    std::iota(hSource, hSource + Elements, 1);
+
+    showData(hSource, Elements);
+
+    cudaMemcpy(dSource, hSource, MemElements, cudaMemcpyHostToDevice);
+
+    hills_steele_scan<<<Block, ThreadsInBlock>>>(dDestination, dSource, [] __device__ (auto a, auto b) { return a + b;});
+
+    cudaMemcpy(hDestination, dDestination, MemElements, cudaMemcpyDeviceToHost);
+
+    showData(hDestination, Elements);
+
+    cudaFree(dDestination);
+    cudaFree(dSource);
+
+    free(hDestination);
+    free(hSource);
+
+
+    cudaDeviceSynchronize();
+}
